ce975f470abad6dad5003f40955d56c21ed969b7
527jgoodman-maker <527JGoodman@frhsd.com>
2026-05-14 11:54:35 -0400

    started working on the objects

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
-let statelist = document.getElementById("state-list");
+let teamList = document.getElementById("team-list");
 
-statelist.onchange = function() {
+teamList.onchange = function() {
     let selected = this.value;
-    document.getElementById('info-division').textContent = stateData[selected].division;
-    document.getElementById('info-owner').textContent = stateData[selected].owner;
-    document.getElementById('info-primary-color').textContent = stateData[selected].primaryColor;
-    document.getElementById('info-current-player').textContent = stateData[selected].currentPlayer;
-    document.getElementById('info-best-all-time-player').textContent = stateData[selected].bestAllTimePlayer;
-    document.getElementById('info-head-coach').textContent = stateData[selected].headCoach;
-    document.getElementById('info-year-founded').textContent = stateData[selected].yearFounded;
-    document.getElementById('info-best-all-time-record').textContent = stateData[selected].bestAllTimeRecord;
-    document.getElementById('info-power-ranking').textContent = stateData[selected].powerRanking;
+    let team = teamData[selected];
+    if (!team) return;
+    document.getElementById('info-name').textContent = team.name;
+    document.getElementById('info-division').textContent = team.division;
+    document.getElementById('info-owner').textContent = team.owner;
+    document.getElementById('info-primary-color').textContent = team.primaryColor;
+    document.getElementById('info-current-player').textContent = team.currentPlayer;
+    document.getElementById('info-best-all-time-player').textContent = team.bestAllTimePlayer;
+    document.getElementById('info-head-coach').textContent = team.headCoach;
+    document.getElementById('info-year-founded').textContent = team.yearFounded;
+    document.getElementById('info-best-all-time-record').textContent = team.bestAllTimeRecord;
+    document.getElementById('info-power-ranking').textContent = team.powerRanking;
     document.getElementById("info-pic").src = `img/${selected}.jpg`;
 }
